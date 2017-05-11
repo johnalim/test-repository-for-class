79911f5db172fd800557cca206f9dfc5811deb65
john.alim <alim.johnmichael@gmail.com>
2017-05-10 21:27:21 -0400

    created hello.js

---
+++ 01/hello.js
@@ -0,0 +1,40 @@
+
+var x ="hello";
+
+console.log(x + " " + "Doge");
+
+var age = 44;
+if (age >=21){
+
+
+console.log("You may legally have a drink.");
+
+if (age <25) {
+
+		console.log("jaegerrrr");
+	}
+if (age >=25 && age <60) {
+
+	console.log("You want an old fashioned");
+}
+if (age >= 60) {
+
+	console.log("You want brandy");
+}
+}else{
+
+console.log("No booze for you!");
+};
+
+
+var counter = 1;
+
+while(counter < 10){
+
+	console.log("woohooooo!" + " " + counter);
+	if (counter % 3 ==0) {
+
+		break
+	}
+	counter++
+}
